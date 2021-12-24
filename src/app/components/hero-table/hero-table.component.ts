f47b7833b87cfc09260bc4182662720ae2a531bf
Georgi Dimitrov <georgedimmitrov@gmail.com>
2021-12-24 20:16:56 +0200

    Initial commit

--- src/app/components/hero-table/hero-table.component.ts
+++ src/app/components/hero-table/hero-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero, HeroService } from '../../services/hero.service';
+import { combineLatest, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
     selector: 'rx-hero-table',
@@ -7,13 +9,55 @@ import { Hero, HeroService } from '../../services/hero.service';
     styleUrls: ['./hero-table.component.scss'],
 })
 export class HeroTableComponent implements OnInit {
-    heroes: Hero[];
+    showSpinner = false;
 
-    constructor(public hero: HeroService) {
-        hero.heroes$.subscribe(heroes => {
-            this.heroes = heroes;
-        });
-    }
+    vm$ = combineLatest([
+        this.hero.heroes$,
+        this.hero.search$,
+        this.hero.userPage$,
+        this.hero.limit$,
+        this.hero.totalResults$,
+        this.hero.totalPages$,
+        this.hero.loading$,
+    ]).pipe(
+        map(
+            ([
+                heroes,
+                search,
+                page,
+                limit,
+                totalResults,
+                totalPages,
+                loading,
+            ]) => {
+                return {
+                    heroes,
+                    search,
+                    page,
+                    limit,
+                    totalResults,
+                    totalPages,
+                    loading,
+                    disableNext: totalPages === page,
+                    disablePrev: page === 1,
+                };
+            },
+        ),
+    );
+
+    constructor(public hero: HeroService) {}
 
     ngOnInit() {}
+
+    doSearch(event: any) {
+        this.hero.doSearch(event.target.value);
+    }
+
+    movePageBy(moveBy: number) {
+        this.hero.movePageBy(moveBy);
+    }
+
+    setLimit(limit: number) {
+        this.hero.setLimit(limit);
+    }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { debounceTime, map, shareReplay, switchMap, tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 export interface Hero {
@@ -42,22 +42,85 @@ const LIMIT_MID = 25;
 const LIMIT_HIGH = 100;
 const LIMITS = [LIMIT_LOW, LIMIT_MID, LIMIT_HIGH];
 
+export const DEFAULT_LIMIT = LIMIT_HIGH;
+export const DEFAULT_SEARCH = '';
+export const DEFAULT_PAGE = 0;
+
 @Injectable({
     providedIn: 'root',
 })
 export class HeroService {
     limits = LIMITS;
 
-    heroes$: Observable<Hero[]> = this.http
-        .get(HERO_API, {
-            params: {
+    private searchBS = new BehaviorSubject<string>(DEFAULT_SEARCH);
+    private limitBS = new BehaviorSubject<number>(DEFAULT_LIMIT);
+    private pageBS = new BehaviorSubject<number>(DEFAULT_PAGE);
+    private loadingBS = new BehaviorSubject(false);
+
+    search$ = this.searchBS.asObservable();
+    limit$ = this.limitBS.asObservable();
+    page$ = this.pageBS.asObservable();
+    loading$ = this.loadingBS.asObservable();
+
+    userPage$ = this.pageBS.pipe(map(page => page + 1));
+
+    private params$ = combineLatest([
+        this.searchBS,
+        this.limitBS,
+        this.pageBS,
+    ]).pipe(
+        map(([searchTerm, limit, page]) => {
+            const params: any = {
                 apikey: environment.MARVEL_API.PUBLIC_KEY,
-                limit: `${LIMIT_LOW}`,
-                // nameStartsWith: 'iron', // once we have search
-                offset: `${0}`, // page * limit
-            },
-        })
-        .pipe(map((res: any) => res.data.results));
+                limit: `${limit}`,
+                offset: `${page * limit}`,
+            };
+
+            if (searchTerm.length) {
+                params.nameStartsWith = searchTerm;
+            }
+            return params;
+        }),
+    );
+
+    private heroesResponse$ = this.params$.pipe(
+        debounceTime(500),
+        tap(() => this.loadingBS.next(true)),
+        switchMap(_params =>
+            this.http.get(HERO_API, {
+                params: _params,
+            }),
+        ),
+        tap(() => this.loadingBS.next(false)),
+        shareReplay(1),
+    );
+
+    totalResults$: Observable<number> = this.heroesResponse$.pipe(
+        map((res: any) => res.data.total),
+    );
+
+    heroes$: Observable<Hero[]> = this.heroesResponse$.pipe(
+        map((res: any) => res.data.results),
+    );
+
+    totalPages$ = combineLatest([this.totalResults$, this.limitBS]).pipe(
+        map(([totalResults, limit]) => Math.ceil(totalResults / limit)),
+    );
 
     constructor(private http: HttpClient) {}
+
+    doSearch(term: string) {
+        this.searchBS.next(term);
+        this.pageBS.next(DEFAULT_PAGE);
+    }
+
+    movePageBy(moveBy: number) {
+        const currentPage = this.pageBS.getValue();
+        this.pageBS.next(currentPage + moveBy);
+    }
+
+    setLimit(newLimit: number) {
+        this.limitBS.next(newLimit);
+        this.pageBS.next(DEFAULT_PAGE);
+    }
 }
